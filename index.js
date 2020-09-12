e61e35d806ca311d1af86e0c7adc0ee49b99b5c1
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-12 13:44:06 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,37 +19,7 @@ for(const file of commandFiles){
 
     client.commands.set(command.name, command);
 }
-function getPic() {
-    https
-    .get("https://api.nasa.gov/planetary/apod?api_key=QBj6HhO1zMguPDxu9DfKpjqmmknQS6PgP6y7h0Sk&date="+year + "-" + month + "-" + date, resp => {
-        let data = " ";
-        resp.on("data", chunk => {
-            data += chunk;
-        })
 
-        resp.on("end", () => {
-            let url = JSON.parse(data).hdurl;
-            client.guilds.cache.forEach((guild) => {
-                setInterval (function() {
-                const channel = guild.channels.cache.find(channel => channel.name === "nasa-pic-of-the-day")
-                const exampleEmbed = new Discord.MessageEmbed()
-                .setColor('#3A7DC6')
-                .setTitle('Astronomy Picture of the Day!')
-                .setDescription('Todays Picture:')
-                .setThumbnail('https://lh3.googleusercontent.com/proxy/n5sEAzXHCzfxMROLim39lpnLfTnUhI-W-f2RIW7aB7iKKzUQidbgaJHRzapeD8T6PpciNMvxkPVmBAsv281u_GvGiImKMpldlSQFPvsnuiXDgjUkrDA')
-                .setImage(url)
-                .setTimestamp()
-
-                channel.send(exampleEmbed);
-            }, 24*60*60*1000)}); 
-        });
-    })
-    .on("error", err => {
-        console.log("Error: " + err.message);
-    });
-
-
-}
 
 client.on("ready", function(){
     client.user.setActivity('the stars....🌌', { type: 'WATCHING' });
@@ -68,8 +38,7 @@ client.on("ready", function(){
     
         if(command === 'help'){
             client.commands.get('help').execute(message, args);
-        } else if (command == 'pic') {
-            client.commands.get('pic').execute(message, args)
+        } else if (command == 'ping') {
         }
     })
     
