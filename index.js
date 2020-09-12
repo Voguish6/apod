1ade5edb39653c0c8c24e14d07d59bf04268dd72
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-11 20:11:30 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,22 @@ const client = new Discord.Client();
 const http = require("http");
 const https = require("https");
 const { isContext } = require('vm');
+const fs = require('fs');
+const prefix = "?"
 var token = process.env.BOT_TOKEN;
 let date_ob = new Date();
 let date = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 let year = date_ob.getFullYear();
 
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
 function getPic() {
     https
     .get("https://api.nasa.gov/planetary/apod?api_key=QBj6HhO1zMguPDxu9DfKpjqmmknQS6PgP6y7h0Sk&date="+year + "-" + month + "-" + date, resp => {
@@ -49,6 +59,17 @@ client.on("ready", function(){
     getPic(); 
     });
 
+
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.client) return;
+    const args = message.content.slice(prefix.length).split(/ +/);
+
+    if (command === 'ping') {
+        client.commands.get('ping').execute(message, args);
+    } else if (command == 'youtube') {
+
+    }
+})
 
 
 client.login(token)
