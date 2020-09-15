329314825b1ea99e0a7e7a89fcbcb8dbeacd0077
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-15 08:09:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -19,6 +19,32 @@ for(const file of commandFiles){
 
     client.commands.set(command.name, command);
 }
+function getPic(channel) {
+    https
+    .get("https://api.nasa.gov/planetary/apod?api_key=QBj6HhO1zMguPDxu9DfKpjqmmknQS6PgP6y7h0Sk&date="+year + "-" + month + "-" + date, resp => {
+        let data = " ";
+        resp.on("data", chunk => {
+            data += chunk;
+        })
+
+        resp.on("end", () => {
+            let url = JSON.parse(data).hdurl;
+            client.guilds.cache.forEach((guild) => {
+
+                const exampleEmbed = new Discord.MessageEmbed()
+                .setColor('#3A7DC6')
+                .setTitle('Astronomy Picture of the Day!')
+                .setDescription('Todays Picture:')
+                .setThumbnail('https://lh3.googleusercontent.com/proxy/n5sEAzXHCzfxMROLim39lpnLfTnUhI-W-f2RIW7aB7iKKzUQidbgaJHRzapeD8T6PpciNMvxkPVmBAsv281u_GvGiImKMpldlSQFPvsnuiXDgjUkrDA')
+                .setImage(url)
+                .setTimestamp()
+
+                channel.send(exampleEmbed);}); 
+        });
+    })
+    .on("error", err => {
+        console.log("Error: " + err.message);
+    })}
 
 
 client.on("ready", function(){
@@ -26,8 +52,12 @@ client.on("ready", function(){
     console.log(`the client becomes ready to start`);
 	console.log(`I am ready! Logged in as ${client.user.tag}!`);
     console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);
-    getPic(); 
-    });
+    const nasaChannel = guild.channels.cache.find(channel => channel.name === "nasa-pic-of-the-day")
+    setInterval(function() {
+        getPic(nasaChannel)
+    }, 24*60*60*1000)
+});
+
 
 
     client.on('message', message =>{
@@ -39,6 +69,9 @@ client.on("ready", function(){
         if(command === 'help'){
             client.commands.get('help').execute(message, args);
         } else if (command == 'ping') {
+        } else if (command == 'pic') {
+            var messagechannel = message.channel.cache
+            getPic(messagechannel)
         }
     })
     
