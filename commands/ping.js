146c24c8f703d643904831f36fce96c15950e3ba
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-15 08:26:56 -0500

    Add files via upload

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,16 @@
+const { Channel } = require("discord.js")
+
 module.exports = {
     name: 'ping',
     description: 'Responds',
     execute(message, args){
-        channel.send("pong!")
+        const exampleEmbed = new Discord.MessageEmbed()
+        .setColor('#3A7DC6')
+        .setTitle('Astronomy Picture of the Day!')
+        .setDescription('Todays Picture:')
+        .setThumbnail('https://lh3.googleusercontent.com/proxy/n5sEAzXHCzfxMROLim39lpnLfTnUhI-W-f2RIW7aB7iKKzUQidbgaJHRzapeD8T6PpciNMvxkPVmBAsv281u_GvGiImKMpldlSQFPvsnuiXDgjUkrDA')
+        .setImage(url)
+        .setTimestamp()
+        channel.send(exampleEmbed)
     }
 }
