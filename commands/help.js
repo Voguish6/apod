e8576a9e318ff5151c940b87f7e77a72e57b89b4
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-17 17:44:51 -0500

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,16 @@ module.exports= {
     name: 'help',
     description: 'this is a ping command.',
     execute(message, args){
-        message.channel.send('pong!')
+        const helpEmbed = new Discord.MessageEmbed()
+        .setColor('#3A7DC6')
+        .setTitle('Help!')
+        .setDescription('Commands:')
+        .setThumbnail('https://i.ibb.co/2gt7TP3/Pin-Clipart-com-nasa-clipart-1116102.png')
+        .addFields({
+            name: '?pic', value: 'Sends the Astronomy Picture of the Day instantly.',
+            name: '?help', value: 'Sends this embed!'
+        })
+        .setTimestamp()
+        message.channel.send(helpEmbed)
     }
 }
