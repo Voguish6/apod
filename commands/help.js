ff1d83f142171b4aec8ab54cdd718bdd982d382d
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-17 18:30:49 -0500

    Add files via upload

--- commands/help.js
+++ commands/help.js
@@ -5,12 +5,12 @@ module.exports= {
         const helpEmbed = new Discord.MessageEmbed()
         .setColor('#3A7DC6')
         .setTitle('Help!')
-        .setDescription('**Commands**:')
+        .setDescription('***Commands***:')
         .setThumbnail('https://i.ibb.co/2gt7TP3/Pin-Clipart-com-nasa-clipart-1116102.png')
-        .addFields({
-            name: '?pic', value: 'Sends the Astronomy Picture of the Day instantly.',
-            name: '?help', value: 'Sends this embed!',
-        })
+        .addFields(
+            {name: '?pic', value: 'Sends the Astronomy Picture of the Day instantly.'},
+            {name: '?help', value: 'Sends this message!'}
+        )
         .setTimestamp()
         message.channel.send(helpEmbed)
     }
