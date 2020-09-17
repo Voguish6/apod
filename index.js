5b736072069c202aeb7d9c1b367a196f1ea3f1c8
Voguish6 <66391485+Voguish6@users.noreply.github.com>
2020-09-17 17:25:28 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ client.on("ready", function(){
     console.log(`the client becomes ready to start`);
 	console.log(`I am ready! Logged in as ${client.user.tag}!`);
     console.log(`Bot has started, with ${client.users.cache.size} users, in ${client.channels.cache.size} channels of ${client.guilds.cache.size} guilds.`);  
+    dailyMSG()
 });
 
 
